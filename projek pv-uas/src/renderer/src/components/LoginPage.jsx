a1036a9c4ddbbe7efb4500a75f61ceee5d733b95
Bryan Marvel <bryan.m24@mhs.istts.ac.id>
2025-11-25 13:21:26 +0700

    Update LoginPage.jsx
    HALO

--- projek pv-uas/src/renderer/src/components/LoginPage.jsx
+++ projek pv-uas/src/renderer/src/components/LoginPage.jsx
@@ -10,7 +10,7 @@ export default function LoginPage() {
 
   useEffect(() => {
     if (user) {
-      if (user.role == "admin") {
+      if (user.role == " admin") {
         navigate('/admin');
       } else if (user.role == "company") {
         navigate('/company');
@@ -61,3 +61,5 @@ export default function LoginPage() {
     </div>
   );
 }
+
+halo
